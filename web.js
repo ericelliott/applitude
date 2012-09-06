474b16227c617f682479aba7731e5bb8a1635f2a
Eric Elliott <dilvie@dilvie.com>
2012-09-05 17:42:34 -0700

    Update web.js

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ http.createServer(function(request, response) {
       return;
     }
 
-  if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+  if (fs.statSync(filename).isDirectory()) filename += 'test/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
       if(err) {        
